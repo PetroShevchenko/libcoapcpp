203b5882e6790f4344bfa404bf95e27cb889116d
Petro Shevchenko <shevchenko.p.i@gmail.com>
2021-05-05 13:52:29 +0300

    Fixed bug in Socket class

--- api/socket.h
+++ api/socket.h
@@ -44,7 +44,7 @@ struct Socket
     virtual ssize_t recvfrom(void * buf, std::size_t len, SocketAddress * addr, std::error_code &ec) = 0;
     virtual void bind(const SocketAddress * addr, std::error_code &ec) = 0;
     virtual Socket * accept(std::error_code * ec = nullptr) = 0;
-    virtual void listen(std::error_code &ec, int max_connections_in_queue = 1);
+    virtual void listen(std::error_code &ec, int max_connections_in_queue = 1) = 0;
     virtual void set_blocking(bool blocking, std::error_code &ec) = 0;
     virtual void set_timeout(size_t timeout, std::error_code &ec) = 0;
     virtual void setsockoption(int level, int option_name, const void *option_value, std::size_t option_len, std::error_code &ec) = 0;
